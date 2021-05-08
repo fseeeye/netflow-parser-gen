85a3dfce3d30cfe6d63d82aeec2613daf47fdaf1
starliiit <esu.eru.sl@gmail.com>
2021-05-08 14:59:15 +0800

    test enum

--- src/enum.test.ts
+++ src/enum.test.ts
@@ -55,14 +55,13 @@ test('enum definition with reference', () => {
     const functionCodeField = new NumericField('function_code', PrimitiveNumericType.u8)
     const variants = [
         new StructEnumVariant(
-            'ReadWriteMultipleRegisters',
+            'WriteFileRecordSubRequest',
             [
-                new NumericField('read_start_address', PrimitiveNumericType.be_u16),
-                new NumericField('read_count', PrimitiveNumericType.be_u16),
-                new NumericField('write_start_address', PrimitiveNumericType.be_u16),
-                new NumericField('write_count', PrimitiveNumericType.be_u16),
-                new NumericField('write_byte_count', PrimitiveNumericType.u8),
-                new BytesReferenceField('write_register_values', 'write_byte_count'),
+                new NumericField('ref_type', PrimitiveNumericType.u8),
+                new NumericField('file_number', PrimitiveNumericType.be_u16),
+                new NumericField('record_number', PrimitiveNumericType.be_u16),
+                new NumericField('record_len', PrimitiveNumericType.be_u16),
+                new BytesReferenceField('record_data', 'record_len'),
             ],
             enumName,
         ),
