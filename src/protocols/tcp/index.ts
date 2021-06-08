d70183d5364cf2f7faa8f7bef5fad171a1907611
starliiit <esu.eru.sl@gmail.com>
2021-06-08 15:23:21 +0800

    style

--- src/protocols/tcp/index.ts
+++ src/protocols/tcp/index.ts
@@ -6,26 +6,8 @@ import {
 } from "../../api/input"
 import { BitNumericFieldGroup } from "../../field/bit-field"
 import { ConditionImpl, OptionField } from "../../field/option"
-import { StructField } from "../../field/struct"
-import { BitFieldStruct } from "../../types/bit-struct"
 import { Struct } from "../../types/struct"
 import { Protocol } from ".././generator"
-
-// const tcpHeader = new BitFieldStruct(
-//     'TcpHeader',
-//     [
-//         bitNumeric('src_port', 16, 'be_u16'),
-//         bitNumeric('dst_port', 16, 'be_u16'),
-//         bitNumeric('seq', 32, 'be_u32'),
-//         bitNumeric('ack', 32, 'be_u32'),
-//         bitNumeric('header_length', 4, 'u8'),
-//         bitNumeric('reserved', 3, 'u8'),
-//         bitNumeric('flags', 9, 'be_u16'),
-//         bitNumeric('window_size', 16, 'be_u16'),
-//         bitNumeric('checksum', 16, 'be_u16'),
-//         bitNumeric('urgent_pointer', 16, 'be_u16'),
-//     ]
-// )
 
 const tcpOptions = new OptionField(
     'options',
